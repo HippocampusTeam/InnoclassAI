37806e05553e1e6fac641428653c0e6274c8c1d5
DablSi <duck.shelter@gmail.com>
2021-07-08 00:02:00 +0300

    Added a part of hybrid's comments

--- hybrid.py
+++ hybrid.py
@@ -4,30 +4,32 @@ def softsign(x):
 
 
 class NeuralNetwork:
-    def __init__(self, size):
-        self.network = list()
-        self.biases = list()
-        for layer_size in size:
-            self.network.append([0] * layer_size)
-            self.biases.append([0] * layer_size)
+    def __init__(self, size):  # Создааем объект класса NeuralNetwork, передавая ему список размеров каждого слоя
+        self.layers = list()  # Список слоев нейронов (каждый слой - это список значений нейронов)
+        self.biases = list()  # Список bias'ов нейронов (структура такая же, как и у layers)
+        for layer_size in size:  # Проходимся по списку размеров слоев
+            self.layers.append([0] * layer_size)  # Создаем новый слой данного размера
+            self.biases.append([0] * layer_size)  # Создаем список bias'ов для этого слоя
 
-        self.weights = list()
-        self.weights.append(list())
-        for layer in range(1, len(self.network)):
+        self.weights = list()  # Создаем список весов
+        self.weights.append(list())  #
+        for layer in range(1, len(self.layers)):
             self.weights.append(list())
-            for neuron in range(len(self.network[layer])):
+            for neuron in range(len(self.layers[layer])):
                 self.weights[layer].append(list())
-                for previous in range(len(self.network[layer - 1])):
+                for previous_neuron in range(len(self.layers[layer - 1])):
                     self.weights[layer][neuron].append(0)
 
+    # Устанавливаем веса
     def set_weights(self, layer, weights):
         counter = 0
 
         # Проходим через все веса предыдущего слоя
-        for previous_neuron in range(len(self.network[layer - 1])):
+        for previous_neuron in range(len(self.layers[layer - 1])):
             # Проходим через все веса текущего слоя
-            for current_neuron in range(len(self.network[layer])):
-                self.weights[layer][current_neuron][previous_neuron] = weights[counter]  # Устанавливаем значение веса на соответствующее по счету
+            for current_neuron in range(len(self.layers[layer])):
+                self.weights[layer][current_neuron][previous_neuron] = weights[counter]
+                # Устанавливаем значение веса на соответствующее по счету
                 counter += 1
 
         return self.weights
@@ -37,10 +39,10 @@ class NeuralNetwork:
         self.biases[layer] = biases
 
     def calculate(self, input):
-        self.network[0] = input
-        for layer in range(1, len(self.network)):
-            for neuron in range(len(self.network[layer])):
-                for previous in range(len(self.network[layer - 1])):
-                    self.network[layer][neuron] += self.weights[layer][neuron][previous] * self.network[layer - 1][previous]
-                self.network[layer][neuron] = softsign(self.network[layer][neuron] + self.biases[layer][neuron])
+        self.layers[0] = input
+        for layer in range(1, len(self.layers)):
+            for neuron in range(len(self.layers[layer])):
+                for previous in range(len(self.layers[layer - 1])):
+                    self.layers[layer][neuron] += self.weights[layer][neuron][previous] * self.layers[layer - 1][previous]
+                self.layers[layer][neuron] = softsign(self.layers[layer][neuron] + self.biases[layer][neuron])
 
