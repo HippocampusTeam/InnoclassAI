7fe45c783c105d438ff48b3f0307b4c9359b84a4
DablSi <duck.shelter@gmail.com>
2021-07-09 21:17:57 +0300

    Redone the testing system

--- test.py
+++ test.py
@@ -1,63 +1,117 @@
-import unittest
 from hybrid import NeuralNetwork
+from sys import exc_info
+from traceback import extract_tb
 
-calculate_msg = 'Что-то не так с вычислением вывода нейронной сети (функция calculate). Убедитесь, что: веса последнего слоя выставляются правильно, вы используете функцию активации, вы правильно вычисляете значения ВСЕХ нейронов, вы не забываете умножать на веса и не перепутали индексы нейронов'
+calculate_msg = 'Что-то не так с вычислением вывода нейронной сети (функция calculate). Убедитесь, что: веса ' \
+                'последнего слоя выставляются правильно, вы используете функцию активации, вы правильно вычисляете ' \
+                'значения ВСЕХ нейронов, вы не забываете умножать на веса и не перепутали индексы нейронов. '
+eps = 1e-3
 
 
 # Test cases to test NN's methods
-class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.network = NeuralNetwork([5, 3, 3])
-
-    def test_set_weights1(self):
+def test_set_weights1():
+    try:
+        network = NeuralNetwork([5, 3, 3])
         output = ([[2, 1, 3], [2, 1, 2], [3, 4, 5]])
-        self.assertEqual(self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5]), output,
-                         msg='Что-то не так с установкой весов во третьем слое. Проверьте функцию set_weights')
+        assert network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5]) == output
+    except Exception as e:
+        print('Тест #1: Что-то не так с установкой весов во третьем слое. Проверьте функцию set_weights.')
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #1 пройден!\n')
 
-    def test_set_weights2(self):
-        output = ([[0.546, -4, 355678875865], [4, 678, 5], [345.76867, 6, 7], [8, -325.6, 6456.7], [4, 7, 87]])
-        self.assertEqual(self.network.set_weights(1,
-            [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87]),
-            output, msg='Что-то не так с установкой весов во втором слое. Проверьте функцию set_weights')
 
-    def test_calculate0(self):
-        output = self.network.calculate([6, -3, 4.6, -5.7, 0])
-        self.assertEqual(output, [0, 0, 0],
-                         msg='Что-то не так с вычислением вывода нейронной сети (функция calculate). Убедитесь, '
-                             'что все веса изначально равны 0')
+def test_set_weights2():
+    output = ([[0.546, -4, 355678875865], [4, 678, 5], [345.76867, 6, 7], [8, -325.6, 6456.7], [4, 7, 87]])
+    try:
+        network = NeuralNetwork([5, 3, 3])
+        assert network.set_weights(1,
+                                   [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4,
+                                    7, 87]) == output
+    except Exception as e:
+        print('Тест #2: Что-то не так с установкой весов во третьем слое. Проверьте функцию set_weights.')
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #2 пройден!\n')
 
-    def test_calculate1(self):
-        self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5])
-        self.network.set_biases(1, [3, -2, 5])
-        eps = 1e-3
-        output = self.network.calculate([6, 3, 4.6, 5.7, 0])
-        self.assertLessEqual(abs(output[0] - 0.727), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[1] - 0.773), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[2] - 0.835), eps, msg=calculate_msg)
 
-    def test_calculate2(self):
-        self.network.set_weights(1,
-            [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87, 56, 34, 67., 34., 908])
-        self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5, 3, -2, 5])
-        eps = 1e-3
-        output = self.network.calculate([6, 3, 4.6, 5.7, 0])
-        self.assertLessEqual(abs(output[0] - 0.874), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[1] - 0.857), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[2] - 0.908), eps, msg=calculate_msg)
+def test_calculate0():
+    try:
+        network = NeuralNetwork([5, 3, 3])
+        output = network.calculate([6, -3, 4.6, -5.7, 0])
+        assert output == [0, 0, 0]
+    except Exception as e:
+        print('Тест #3: Что-то не так с вычислением вывода нейронной сети (функция calculate). Убедитесь, '
+              'что все веса изначально равны 0.')
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #3 пройден!\n')
 
-    def test_calculate3(self):
-        self.network.set_weights(1,
-            [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87])
-        self.network.set_biases(0, [56, 34, 67., 34., 908])
-        self.network.set_weights(2, [8, 9.96, -3, 0.062, -0.7, 82, 63, -4, -58])
-        self.network.set_biases(1, [0.3, -2, 51])
-        eps = 1e-3
-        output = self.network.calculate([6, 3, 4.6, 5.7, 0])
-        self.assertLessEqual(abs(output[0] - 0.986), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[1] - 0.840), eps, msg=calculate_msg)
-        self.assertLessEqual(abs(output[2] - 0.953), eps, msg=calculate_msg)
+
+def test_calculate1():
+    try:
+        network = NeuralNetwork([5, 3, 3])
+        network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5])
+        network.set_biases(1, [3, -2, 5])
+        output = network.calculate([6, 3, 4.6, 5.7, 0])
+        assert abs(output[0] - 0.727) <= eps
+        assert abs(output[1] - 0.773) <= eps
+        assert abs(output[2] - 0.835) <= eps
+    except Exception as e:
+        print('Тест #4:', calculate_msg)
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #4 пройден!\n')
+
+
+def test_calculate2():
+    try:
+        network = NeuralNetwork([5, 3, 3])
+        network.set_weights(1,
+                            [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87,
+                             56, 34, 67., 34., 908])
+        network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5, 3, -2, 5])
+        output = network.calculate([6, 3, 4.6, 5.7, 0])
+        assert abs(output[0] - 0.874) <= eps
+        assert abs(output[1] - 0.857) <= eps
+        assert abs(output[2] - 0.908) <= eps
+    except Exception as e:
+        print('Тест #5:', calculate_msg)
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #5 пройден!\n')
+
+
+def test_calculate3():
+    try:
+        network = NeuralNetwork([5, 3, 3])
+        network.set_weights(1,
+                            [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87])
+        network.set_biases(0, [56, 34, 67., 34., 908])
+        network.set_weights(2, [8, 9.96, -3, 0.062, -0.7, 82, 63, -4, -58])
+        network.set_biases(1, [0.3, -2, 51])
+        output = network.calculate([6, 3, 4.6, 5.7, 0])
+        assert abs(output[0] - 0.986) <= eps
+        assert abs(output[1] - 0.840) <= eps
+        assert abs(output[2] - 0.953) <= eps
+    except Exception as e:
+        print('Тест #6:', calculate_msg)
+        if not (e.__class__ is AssertionError):
+            print('Причина:', e.args[0], 'в строке номер', str(extract_tb(exc_info()[2])[1][1]) + '.\n')
+    else:
+        print('Тест #6 пройден!\n')
 
 
 # Executing the tests in the above test case class
 if __name__ == "__main__":
-    unittest.main()
+    test_set_weights1()
+    test_set_weights2()
+    test_calculate0()
+    test_calculate1()
+    test_calculate2()
+    test_calculate3()
