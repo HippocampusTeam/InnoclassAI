f0d1d8d1fad0c4403c956f054abf9712ca9b81c9
DablSi <duck.shelter@gmail.com>
2021-07-09 19:55:36 +0300

    Fixed unittests

--- hybrid.py
+++ hybrid.py
@@ -5,44 +5,46 @@ def softsign(x):
 
 class NeuralNetwork:
     def __init__(self, size):  # Создааем объект класса NeuralNetwork, передавая ему список размеров каждого слоя
-        self.layers = list()  # Список слоев нейронов (каждый слой - это список значений нейронов)
-        self.biases = list()  # Список bias'ов нейронов (структура такая же, как и у layers)
+        self.neurons = list()  # Список слоев нейронов (каждый слой - это список значений нейронов)
+        self.biases = list()  # Список bias'ов нейронов (структура такая же, как и у neurons)
         for layer_size in size:  # Проходимся по списку размеров слоев
-            self.layers.append([0] * layer_size)  # Создаем новый слой данного размера
+            self.neurons.append([0] * layer_size)  # Создаем новый слой данного размера
             self.biases.append([0] * layer_size)  # Создаем список bias'ов для этого слоя
 
         self.weights = list()  # Создаем список весов
-        self.weights.append(list())  #
-        for layer in range(1, len(self.layers)):
+        self.weights.append(list())
+        for layer in range(1, len(self.neurons)):
             self.weights.append(list())
-            for neuron in range(len(self.layers[layer])):
+            for previous_neuron in range(len(self.neurons[layer - 1])):
                 self.weights[layer].append(list())
-                for previous_neuron in range(len(self.layers[layer - 1])):
-                    self.weights[layer][neuron].append(0)
+                for current_neuron in range(len(self.neurons[layer])):
+                    self.weights[layer][previous_neuron].append(0)
 
     # Устанавливаем веса
     def set_weights(self, layer, weights):
         counter = 0
 
         # Проходим через все веса предыдущего слоя
-        for previous_neuron in range(len(self.layers[layer - 1])):
+        for previous_neuron in range(len(self.neurons[layer - 1])):
             # Проходим через все веса текущего слоя
-            for current_neuron in range(len(self.layers[layer])):
-                self.weights[layer][current_neuron][previous_neuron] = weights[counter]
+            for current_neuron in range(len(self.neurons[layer])):
+                self.weights[layer][previous_neuron][current_neuron] = weights[counter]
                 # Устанавливаем значение веса на соответствующее по счету
                 counter += 1
 
-        return self.weights
+        return self.weights[layer]
 
     # Устанавливаем bias'ы
     def set_biases(self, layer, biases):
         self.biases[layer] = biases
 
     def calculate(self, input):
-        self.layers[0] = input
-        for layer in range(1, len(self.layers)):
-            for neuron in range(len(self.layers[layer])):
-                for previous in range(len(self.layers[layer - 1])):
-                    self.layers[layer][neuron] += self.weights[layer][neuron][previous] * self.layers[layer - 1][previous]
-                self.layers[layer][neuron] = softsign(self.layers[layer][neuron] + self.biases[layer][neuron])
+        self.neurons[0] = input
+        for layer in range(1, len(self.neurons)):
+            for neuron in range(len(self.neurons[layer])):
+                for previous in range(len(self.neurons[layer - 1])):
+                    self.neurons[layer][neuron] += self.weights[layer][previous][neuron] * self.neurons[layer - 1][previous]
+                self.neurons[layer][neuron] = softsign(self.neurons[layer][neuron] + self.biases[layer][neuron])
+
+        return self.neurons[-1]
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import NeuralNetwork
+from hybrid import NeuralNetwork
 
 calculate_msg = 'Что-то не так с вычислением вывода нейронной сети (функция calculate). Убедитесь, что: веса последнего слоя выставляются правильно, вы используете функцию активации, вы правильно вычисляете значения ВСЕХ нейронов, вы не забываете умножать на веса и не перепутали индексы нейронов'
 
@@ -11,12 +11,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_set_weights1(self):
         output = ([[2, 1, 3], [2, 1, 2], [3, 4, 5]])
-        self.assertEqual(self.network.network[2].set_weights([2, 1, 3, 2, 1, 2, 3, 4, 5]), output,
+        self.assertEqual(self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5]), output,
                          msg='Что-то не так с установкой весов во третьем слое. Проверьте функцию set_weights')
 
     def test_set_weights2(self):
         output = ([[0.546, -4, 355678875865], [4, 678, 5], [345.76867, 6, 7], [8, -325.6, 6456.7], [4, 7, 87]])
-        self.assertEqual(self.network.network[1].set_weights(
+        self.assertEqual(self.network.set_weights(1,
             [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87]),
             output, msg='Что-то не так с установкой весов во втором слое. Проверьте функцию set_weights')
 
@@ -27,8 +27,8 @@ class TestCalculator(unittest.TestCase):
                              'что все веса изначально равны 0')
 
     def test_calculate1(self):
-        self.network.network[2].set_weights([2, 1, 3, 2, 1, 2, 3, 4, 5])
-        self.network.network[1].set_biases([3, -2, 5])
+        self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5])
+        self.network.set_biases(1, [3, -2, 5])
         eps = 1e-3
         output = self.network.calculate([6, 3, 4.6, 5.7, 0])
         self.assertLessEqual(abs(output[0] - 0.727), eps, msg=calculate_msg)
@@ -36,9 +36,9 @@ class TestCalculator(unittest.TestCase):
         self.assertLessEqual(abs(output[2] - 0.835), eps, msg=calculate_msg)
 
     def test_calculate2(self):
-        self.network.network[1].set_weights(
+        self.network.set_weights(1,
             [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87, 56, 34, 67., 34., 908])
-        self.network.network[2].set_weights([2, 1, 3, 2, 1, 2, 3, 4, 5, 3, -2, 5])
+        self.network.set_weights(2, [2, 1, 3, 2, 1, 2, 3, 4, 5, 3, -2, 5])
         eps = 1e-3
         output = self.network.calculate([6, 3, 4.6, 5.7, 0])
         self.assertLessEqual(abs(output[0] - 0.874), eps, msg=calculate_msg)
@@ -46,11 +46,11 @@ class TestCalculator(unittest.TestCase):
         self.assertLessEqual(abs(output[2] - 0.908), eps, msg=calculate_msg)
 
     def test_calculate3(self):
-        self.network.network[1].set_weights(
+        self.network.set_weights(1,
             [0.546, -4, 355678875865, 4, 678, 5, 345.76867, 6, 7, 8, -325.6, 6456.7, 4, 7, 87])
-        self.network.network[0].set_biases([56, 34, 67., 34., 908])
-        self.network.network[2].set_weights([8, 9.96, -3, 0.062, -0.7, 82, 63, -4, -58])
-        self.network.network[1].set_biases([0.3, -2, 51])
+        self.network.set_biases(0, [56, 34, 67., 34., 908])
+        self.network.set_weights(2, [8, 9.96, -3, 0.062, -0.7, 82, 63, -4, -58])
+        self.network.set_biases(1, [0.3, -2, 51])
         eps = 1e-3
         output = self.network.calculate([6, 3, 4.6, 5.7, 0])
         self.assertLessEqual(abs(output[0] - 0.986), eps, msg=calculate_msg)
